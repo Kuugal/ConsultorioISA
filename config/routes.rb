ab68b2cce454341dcd2cff9bb8792e4a1dd44a51
Kuugal <Kuugal@live.com>
2018-10-14 16:22:16 -0500

    devise-full

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'prueba', to: 'home#prueba'
   root 'home#index'
+      devise_for :users
+      as :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+      end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
